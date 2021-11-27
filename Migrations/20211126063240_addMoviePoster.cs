036acd4e8a1e02e86cff5018fe8d17cf8d623f36
Nihil <35506793+Nihilnia@users.noreply.github.com>
2021-11-27 09:44:26 +0300

    v1_done

---
+++ Migrations/20211126063240_addMoviePoster.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectMovie.Migrations
+{
+    public partial class addMoviePoster : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Poster",
+                table: "Movies",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Poster",
+                table: "Movies");
+        }
+    }
+}
---
+++ Models/Database/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectMovie.Models.Database
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Poster { get; set; }
+        public User User { get; set; }
+        public List<Category> Categories { get; set; }
+
+        //FKey
+        public int UserID { get; set; }
+    }
+}
